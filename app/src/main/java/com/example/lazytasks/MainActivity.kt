1991886d4a39bdf0cd6d73b6af7e23ced70689e5
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-17 16:35:36 -0300

    feat: Add LazyColumn implementation

--- app/src/main/java/com/example/lazytasks/MainActivity.kt
+++ app/src/main/java/com/example/lazytasks/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,11 +20,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lazytasks.repository.getTasksByCategory
@@ -50,11 +53,12 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier.padding(40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
             text = stringResource(id = R.string.task_list_title),
             fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
@@ -75,6 +79,12 @@ fun TaskScreen() {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(getTasksByCategory("Work")) { task ->
+                Text(text = "Task: ${task.title}")
+            }
+        }
+
     }
 
 }
